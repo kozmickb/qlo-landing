1e9a129f34fbfaabd1fc329a3f00269aeab4ced0
kozmickb <karo.bonas@gmail.com>
2025-06-02 23:23:47 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
@@ -7,13 +8,7 @@ module.exports = {
         qloOrchid: '#D764D4',
         qloViolet: '#AD5CFF',
       },
-      backgroundImage: {
-        'qlo-gradient': 'linear-gradient(to right, #FF3F8D, #D764D4, #AD5CFF)',
-      },
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
     },
   },
   plugins: [],
-}
+};
