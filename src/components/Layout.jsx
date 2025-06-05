e9dddf69cf629007deddc4b0ff54b4cae2993b8d
kozmickb <karo.bonas@gmail.com>
2025-06-06 00:11:07 +0100

    Add files via upload

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,2 @@
 import React from 'react';
-import Hero from './Hero';
-import Features from './Features';
-import Screenshots from './Screenshots';
-import Footer from './Footer';
-
-export default function Layout() {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Hero />
-      <Features />
-      <Screenshots />
-      <Footer />
-    </div>
-  );
-}
+export default function Layout() { return <div>Hello Qlo Landing</div>; }
