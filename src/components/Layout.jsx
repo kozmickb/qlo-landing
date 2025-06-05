202be1576cd3830a33ad8546792efde257e169cb
kozmickb <karo.bonas@gmail.com>
2025-06-05 22:48:09 +0100

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,14 @@
+import React from 'react';
 import Hero from './Hero';
 import Features from './Features';
+import Footer from './Footer';
 
 export default function Layout() {
   return (
     <main className="bg-zinc-950 text-white">
       <Hero />
       <Features />
+      <Footer />
     </main>
   );
 }
