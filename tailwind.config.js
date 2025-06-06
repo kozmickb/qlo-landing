49fc8209185a50a252b8890810d97265da76d901
kozmickb <karo.bonas@gmail.com>
2025-06-06 10:57:28 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}"
-  ],
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        jakarta: ["'Plus Jakarta Sans'", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
