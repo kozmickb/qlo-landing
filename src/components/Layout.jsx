87b7de27445ec08892127d5fd2a72216d514f38b
kozmickb <karo.bonas@gmail.com>
2025-06-06 11:36:02 +0100

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Hero from './Hero';
 import Features from './Features';
-import CTA from './CTA';
+import DemoCTA from './DemoCTA';
 import Footer from './Footer';
 
 export default function Layout() {
   return (
-    <main className="bg-zinc-950 text-zinc-100 font-jakarta scroll-smooth">
+    <>
       <Hero />
       <Features />
-      <CTA />
+      <DemoCTA />
       <Footer />
-    </main>
+    </>
   );
 }
