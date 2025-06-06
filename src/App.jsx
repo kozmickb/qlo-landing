34049a3c757828c890154be6432a8177b3fb2492
kozmickb <karo.bonas@gmail.com>
2025-06-06 11:35:44 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
-
 import React from 'react';
-import Hero from './components/Hero';
-import Features from './components/Features';
-import Footer from './components/Footer';
+import Layout from './components/Layout';
 
 export default function App() {
-  return (
-    <div className="bg-zinc-950 text-white font-sans scroll-smooth">
-      <Hero />
-      <Features />
-      <Footer />
-    </div>
-  );
+  return <Layout />;
 }
