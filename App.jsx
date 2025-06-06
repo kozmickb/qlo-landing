9c781dbbbe9cdbc2184dea079322c51acedb0097
kozmickb <karo.bonas@gmail.com>
2025-06-06 11:58:03 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BusinessJourney from './components/BusinessJourney';
+import CustomerJourney from './components/CustomerJourney';
+
+export default function App() {
+  return (
+    <div>
+      <BusinessJourney />
+      <CustomerJourney />
+    </div>
+  );
+}
