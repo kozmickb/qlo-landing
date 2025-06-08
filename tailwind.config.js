d2b7da589c40b07a7e06983bd7ac34cba4914e66
kozmickb <karo.bonas@gmail.com>
2025-06-08 14:08:55 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Plus Jakarta Sans', 'sans-serif'],
+      },
+      spacing: {
+        '28': '7rem',
+      },
+    },
   },
   plugins: [],
 }
